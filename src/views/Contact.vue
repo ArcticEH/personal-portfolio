a45f63f4b14756b15f344e385627f973c652e9d3
Brandon Kolar <brandon.kolar@mohawkcollege.ca>
2021-09-03 14:31:03 -0400

    Started adding contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="container">
     <div id="socialMedia">
-      <h2>Want to hear more? <br /> I would love to share<span class="blue">.</span></h2>
+      <h2>
+        Want to hear more? <br />
+        I would love to share<span class="blue">.</span>
+      </h2>
       <div id="socialMediaIcons">
-          <a href="twitter.com"><fab class="socialMediaIcon" :icon="['fab', 'twitter']" /></a>
-          <a href="github.com"><fab class="socialMediaIcon" :icon="['fab', 'github']" /></a>
-          <a href="linkedin.com"><fab class="socialMediaIcon" :icon="['fab', 'linkedin']" /></a>
-          
+        <a href="twitter.com"
+          ><fab class="socialMediaIcon" :icon="['fab', 'twitter']"
+        /></a>
+        <a href="github.com"
+          ><fab class="socialMediaIcon" :icon="['fab', 'github']"
+        /></a>
+        <a href="linkedin.com"
+          ><fab class="socialMediaIcon" :icon="['fab', 'linkedin']"
+        /></a>
+        <a href="linkedin.com"
+          ><fab class="socialMediaIcon" :icon="['fas', 'envelope']"
+        /></a>
       </div>
     </div>
 
     <div id="contactForm">
-      Contact form
+      <h3>Send a message<span class="blue">.</span></h3>
+      <form>
+        <input id="nameInput" type="text" placeholder="Name" />
+        <input id="emailInput" type="email" placeholder="Email" />
+        <textarea type="text" placeholder="Message..." />
+        <button type="submit">Send</button>
+      </form>
     </div>
   </div>
 </template>
@@ -20,43 +37,88 @@ export default {};
 </script>
 <style scoped>
 #container {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    width: 80%;
-    margin-top: 64px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  margin-top: 64px;
 }
 
 #socialMedia {
-    flex: 1;
-}
-
-#socialMediaIcons {
-
+  flex: 1;
 }
 
 .socialMediaIcon {
-    color: white;
-    height: 50px;
-    width: 50px;
-    margin-right: 75px;
+  color: white;
+  height: 50px;
+  width: 50px;
+  margin-right: 75px;
 }
 
 .socialMediaIcon:hover {
-    color: #39ace7;
-
+  color: #39ace7;
 }
 
-
 h2 {
-    font-size: 40px;
+  font-size: 40px;
+}
+
+h3 {
+  font-size: 30px;
+  margin-bottom: 12px;
 }
 
 #contactForm {
-    flex: 1;
-    border: 1px solid white;
-    height: 100%;
+  flex: 1;
+  height: 100%;
 }
 
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+input {
+  height: 25px;
+  width: 100%;
+  background-color: #17171717;
+  color: white;
+  font-size: 25px;
+  padding: 5px;
+  border: 2px solid white;
+  margin-bottom: 12px;
+}
+
+textarea {
+  resize: none;
+  height: 125px;
+  width: 100%;
+  background-color: #17171717;
+  color: white;
+  font-size: 15px;
+  padding: 5px;
+  border: 2px solid white;
+  font-family: arial;
+}
+
+button {
+  height: 50px;
+  width: 100px;
+  font-size: 25px;
+  background-color: #171717;
+  border: none;
+  border-radius: 5px;
+  border: 2px solid white;
+  color: white;
+  cursor: pointer;
+  margin-top: 12px;
+  margin-right: 0px;
+  float: right;
+  align-self: end;
+}
+
+button:hover {
+  background-color: #343434;
+}
 </style>
