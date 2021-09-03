a2691da6a3eb562a11937512ae56d43ae2f356d9
Brandon Kolar <brandon.kolar@mohawkcollege.ca>
2021-09-02 20:36:41 -0400

    added social media icons

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import { createApp } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import App from "./App.vue";
 import router from "./router";
 
 library.add(fas);
+library.add(fab);
 
 createApp(App)
   .use(router)
   .component("fa", FontAwesomeIcon)
+  .component("fab", FontAwesomeIcon)
   .mount("#app");
---
+++ src/views/Contact.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="container">
+    <div id="socialMedia">
+      <h2>Want to hear more? <br /> I would love to share<span class="blue">.</span></h2>
+      <div id="socialMediaIcons">
+          <a href="twitter.com"><fab class="socialMediaIcon" :icon="['fab', 'twitter']" /></a>
+          <a href="github.com"><fab class="socialMediaIcon" :icon="['fab', 'github']" /></a>
+          <a href="linkedin.com"><fab class="socialMediaIcon" :icon="['fab', 'linkedin']" /></a>
+          
+      </div>
+    </div>
+
+    <div id="contactForm">
+      Contact form
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+#container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+    margin-top: 64px;
+}
+
+#socialMedia {
+    flex: 1;
+}
+
+#socialMediaIcons {
+
+}
+
+.socialMediaIcon {
+    color: white;
+    height: 50px;
+    width: 50px;
+    margin-right: 75px;
+}
+
+.socialMediaIcon:hover {
+    color: #39ace7;
+
+}
+
+
+h2 {
+    font-size: 40px;
+}
+
+#contactForm {
+    flex: 1;
+    border: 1px solid white;
+    height: 100%;
+}
+
+</style>
